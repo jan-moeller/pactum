526b6de8db4025d3163538389c30d7f3679304dd
Jan Möller <drag-on@gmx.de>
2025-01-30 21:41:03 +0100

    Implement AssertionKind

---
+++ src/pycontractz/__init__.py
@@ -0,0 +1,1 @@
+from .assertion_kind import AssertionKind
---
+++ src/pycontractz/assertion_kind.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class AssertionKind(Enum):
+    """The kind of contract assertion"""
+
+    pre = 1  # Precondition
+    post = 2  # Postcondition
+    assertion = 3  # Assertion
