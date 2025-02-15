d528e743a51e0145b39973ba2ed0b87e84f9dda2
Jan Möller <drag-on@gmx.de>
2025-02-15 15:19:55 +0100

    Mark imports in __init__.py so they don't raise unused import errors

--- src/pycontractz/__init__.py
+++ src/pycontractz/__init__.py
@@ -1,19 +1,22 @@
-from ._assertion_kind import AssertionKind
-from ._contract_assertion_label import ContractAssertionInfo, ContractAssertionLabel
-from ._contract_violation import ContractViolation
-from ._contract_violation_exception import ContractViolationException
-from ._contract_violation_handler import (
-    get_contract_violation_handler,
-    set_contract_violation_handler,
-    contract_violation_handler,
-    get_contract_evaluation_semantic,
-    set_contract_evaluation_semantic,
-    contract_evaluation_semantic,
-    get_global_contract_assertion_label,
-    set_global_contract_assertion_label,
-    global_contract_assertion_label,
+from ._assertion_kind import AssertionKind  # noqa: F401
+from ._contract_assertion_label import (
+    ContractAssertionInfo,  # noqa: F401
+    ContractAssertionLabel,  # noqa: F401
 )
-from ._evaluation_semantic import EvaluationSemantic
-from ._post import post
-from ._pre import pre
-from ._predicate import Predicate
+from ._contract_violation import ContractViolation  # noqa: F401
+from ._contract_violation_exception import ContractViolationException  # noqa: F401
+from ._contract_violation_handler import (
+    get_contract_violation_handler,  # noqa: F401
+    set_contract_violation_handler,  # noqa: F401
+    contract_violation_handler,  # noqa: F401
+    get_contract_evaluation_semantic,  # noqa: F401
+    set_contract_evaluation_semantic,  # noqa: F401
+    contract_evaluation_semantic,  # noqa: F401
+    get_global_contract_assertion_label,  # noqa: F401
+    set_global_contract_assertion_label,  # noqa: F401
+    global_contract_assertion_label,  # noqa: F401
+)
+from ._evaluation_semantic import EvaluationSemantic  # noqa: F401
+from ._post import post  # noqa: F401
+from ._pre import pre  # noqa: F401
+from ._predicate import Predicate  # noqa: F401
